87d462d89d0a4dbdc5cca6f61ec9152223aca63a
talthur <talthur.ga@gmail.com>
2021-03-16 19:10:28 -0300

    Ajustes

---
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -0,0 +1,33 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.caelum.ingresso.model.descontos.Desconto;
+
+public class Ingresso {
+	private Sessao sessao;
+
+	private BigDecimal preco;
+
+	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+		this.sessao = sessao;
+		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco()).setScale(2, RoundingMode.HALF_UP);
+	}
+	
+	public Sessao getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+}
